b97647d6cb7c9266d7aef145d545126f85693ab3
swanine <haohao1500@foxmail.com>
2025-04-25 16:21:33 +0800

    🛠️ test: add ui tests

---
+++ CogniChatTests/CogniChatTests.swift
@@ -0,0 +1,17 @@
+//
+//  CogniChatTests.swift
+//  CogniChatTests
+//
+//  Created by swanine on 2025/4/15.
+//
+
+import Testing
+@testable import CogniChat
+
+struct CogniChatTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
